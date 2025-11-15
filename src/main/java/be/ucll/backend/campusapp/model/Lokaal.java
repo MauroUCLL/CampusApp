3e985e1f1bcad0784fcc6ec6001bbcf70ae09f2e
MauroUCLL <mauro.deceulaer@student.ucll.be>
2025-11-15 18:08:02 +0100

    start of model

---
+++ src/main/java/be/ucll/backend/campusapp/model/Lokaal.java
@@ -0,0 +1,18 @@
+package be.ucll.backend.campusapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Lokaal {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
