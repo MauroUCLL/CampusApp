1ef0f73acacd26135c745e9e9f2f5cfd6be94f1a
MauroUCLL <mauro.deceulaer@student.ucll.be>
2025-11-15 18:01:59 +0100

    init

---
+++ src/test/java/be/ucll/backend/campusapp/CampusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.backend.campusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
